6e8d6d19263ac36b343d21e88dfc7911a58b7d52
Nazar Novak <nazarnovak@gmail.com>
2020-02-03 16:23:36 +0100

    GA fixes

--- public/gtag.js
+++ public/gtag.js
@@ -1,5 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157516521-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
