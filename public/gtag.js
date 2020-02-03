c243f8508e71e969c1b35ebd28263791eee737b1
Nazar Novak <nazarnovak@gmail.com>
2020-02-03 16:25:37 +0100

    GA fixes 2

--- public/gtag.js
+++ public/gtag.js
@@ -1,7 +1,5 @@
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'UA-157516521-1');
-</script>
+gtag('config', 'UA-157516521-1');
