3a44bcd825d9e79429ac3622986166bfd91ee8e7
Nazar Novak <nazarnovak@gmail.com>
2020-12-13 22:08:36 +0100

    More in 42

--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -8,7 +8,7 @@ export default class NotFound extends React.Component {
           <Nav location={this.props.location} />
           <div className="not-found">
             <h1 className="heading">42</h1>
-            <p className="subheading">Just kidding, nothing meaningful here. You can find meaning in the <a href="/chat">chat</a></p>
+            <p className="subheading">Just kidding, nothing meaningful here. You can find more meaning in the <a href="/chat">chat</a></p>
           </div>
         </div>
     );
